390ceddd7f86138d92609ae4f6ff8d821042d149
Jacob Cell <59305245+Thirstymidget28@users.noreply.github.com>
2024-11-19 19:45:03 -0600

    Setting Botpress scripts to async

--- app/components/Botpress.tsx
+++ app/components/Botpress.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React from "react";
+import Script from "next/script";
 
-const botpress = () => {
+const Botpress = () => {
   return (
     <div>
-      <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
-      <script src="https://files.bpcontent.cloud/2024/10/31/14/20241031140326-5Y66VD48.js"></script>
+      <Script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js" strategy="lazyOnload" />
+      <Script src="https://files.bpcontent.cloud/2024/10/31/14/20241031140326-5Y66VD48.js" strategy="lazyOnload" />
     </div>
   );
 };
 
-export default botpress;
+export default Botpress;
